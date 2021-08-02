291e6d89e6125b9191057a007faeb548d824ac2d
LuckLiu <liuk1991@sina.com>
2021-08-02 21:49:51 +0800

    add Constellation.date_by_constellation

--- lib/constellation.rb
+++ lib/constellation.rb
@@ -69,4 +69,58 @@ module Constellation
     end
   end
 
+  #水瓶座 Aquarius    01/20 - 02/18
+  #双鱼座 Pisces      02/19 - 03/20
+  #白羊座 Aries       03/21 - 04/19
+  #金牛座 Taurus      04/20 - 05/20
+  #双子座 Gemini      05/21 - 06/21
+  #巨蟹座 Cancer      06/22 - 07/22
+  #狮子座 Leo         07/23 - 08/22
+  #处女座 Virgo       08/23 - 09/22
+  #天秤座 Libra       09/23 - 10/23
+  #天蝎座 Scorpio     10/24 - 11/22
+  #射手座 Sagittarius 11/23 - 12/21
+  #摩羯座 Capricorn   12/22 - 01/19
+  def date_by_constellation(cons)
+    case cons
+    when "水瓶座"    then "01/20 - 02/18"
+    when "Aquarius"    then "01/20 - 02/18"
+
+    when "双鱼座"      then "02/19 - 03/20"
+    when "Pisces"      then "02/19 - 03/20"
+
+    when "白羊座"      then "03/21 - 04/19"
+    when "Aries"       then "03/21 - 04/19"
+
+    when "金牛座"      then "04/20 - 05/20"
+    when "Taurus"      then "04/20 - 05/20"
+
+    when "双子座"      then "05/21 - 06/21"
+    when "Gemini"      then "05/21 - 06/21"
+
+    when "巨蟹座"      then "06/22 - 07/22"
+    when "Cancer"      then "06/22 - 07/22"
+
+    when "狮子座"      then "07/23 - 08/22"
+    when "Leo"         then "07/23 - 08/22"
+
+    when "处女座"       then "08/23 - 09/22"
+    when "Virgo"       then "08/23 - 09/22"
+
+    when "天秤座"       then "09/23 - 10/23"
+    when "Libra"       then "09/23 - 10/23"
+
+    when "天蝎座"     then "10/24 - 11/22"
+    when "Scorpio"     then "10/24 - 11/22"
+
+    when "射手座" then "11/23 - 12/21"
+    when "Sagittarius" then "11/23 - 12/21"
+
+    when "摩羯座"   then "12/22 - 01/19"
+    when "Capricorn"   then "12/22 - 01/19"
+    else
+      raise "No this constellation!"
+    end
+  end
+
 end
--- spec/constellation_spec.rb
+++ spec/constellation_spec.rb
@@ -89,9 +89,106 @@ describe Constellation do
         expect(Constellation.constellation("20001222")).to eql constellation
         expect(Constellation.constellation("20000119")).to eql constellation
       end
-
-
     end
   end
 
+  context 'English' do
+    it 'Aquarius' do
+      expect(Constellation.constellation(Date.parse("20000120"), :en)).to eql "Aquarius"
+      expect(Constellation.constellation("20000120", :en)).to eql "Aquarius"
+      expect(Constellation.constellation("20000218", :en)).to eql "Aquarius"
+    end
+
+    it 'Pisces' do
+      constellation = "Pisces"
+
+      expect(Constellation.constellation("20000219", :en)).to eql constellation
+      expect(Constellation.constellation("20000320", :en)).to eql constellation
+    end
+
+    it 'Aries' do
+      constellation = "Aries"
+
+      expect(Constellation.constellation("20000321", :en)).to eql constellation
+      expect(Constellation.constellation("20000419", :en)).to eql constellation
+    end
+
+    it 'Taurus' do
+      constellation = "Taurus"
+
+      expect(Constellation.constellation("20000420", :en)).to eql constellation
+      expect(Constellation.constellation("20000520", :en)).to eql constellation
+    end
+
+    it 'Gemini' do
+      constellation = "Gemini"
+
+      expect(Constellation.constellation("20000521", :en)).to eql constellation
+      expect(Constellation.constellation("20000621", :en)).to eql constellation
+    end
+
+    it 'Cancer' do
+      constellation = "Cancer"
+
+      expect(Constellation.constellation("20000622", :en)).to eql constellation
+      expect(Constellation.constellation("20000722", :en)).to eql constellation
+    end
+
+    it 'Leo' do
+      constellation = "Leo"
+
+      expect(Constellation.constellation("20000723", :en)).to eql constellation
+      expect(Constellation.constellation("20000822", :en)).to eql constellation
+    end
+
+    it 'Virgo' do
+      constellation = "Virgo"
+
+      expect(Constellation.constellation("20000823", :en)).to eql constellation
+      expect(Constellation.constellation("20000922", :en)).to eql constellation
+    end
+
+    it 'Libra' do
+      constellation = "Libra"
+
+      expect(Constellation.constellation("20000923", :en)).to eql constellation
+      expect(Constellation.constellation("20001023", :en)).to eql constellation
+    end
+
+    it 'Scorpio' do
+      constellation = "Scorpio"
+
+      expect(Constellation.constellation("20001024", :en)).to eql constellation
+      expect(Constellation.constellation("20001122", :en)).to eql constellation
+    end
+
+    it 'Sagittarius' do
+      constellation = "Sagittarius"
+
+      expect(Constellation.constellation("20001123", :en)).to eql constellation
+      expect(Constellation.constellation("20001221", :en)).to eql constellation
+    end
+
+    it 'Capricorn' do
+      constellation = "Capricorn"
+
+      expect(Constellation.constellation("20001222", :en)).to eql constellation
+      expect(Constellation.constellation("20000119", :en)).to eql constellation
+    end
+  end
+
+
+  describe '.constellation' do
+    context 'Chinese' do
+      it '水瓶座' do
+        expect(Constellation.date_by_constellation("水瓶座")).to eql "01/20 - 02/18"
+      end
+    end
+
+    context 'English' do
+      it 'Aquarius' do
+        expect(Constellation.date_by_constellation("Aquarius")).to eql "01/20 - 02/18"
+      end
+    end
+  end
 end
